49c3f598eff8d47c7a6a50eeb107d0ade09a0991
Ty Mondragon <tymondragon@gmail.com>
2018-06-15 14:16:28 -0600

    added errors

--- server.js
+++ server.js
@@ -48,4 +48,14 @@ app.delete('footnotes/users/:id/notes/:id', (req, res, next) => {
 })
 
 
-
+//Catch-all
+app.use((req, res, next) => {
+  res.status(404).json({error:{message:'404 Not Found'}})
+})
+//All other error routes
+// next({status:500, message:'something went wrong'})
+//
+app.use((err, req, res, next) => {
+  res.status(err.status).json({error:err})
+})
+app.listen(port, listener)
