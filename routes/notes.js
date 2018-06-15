124fa0bd30ecb6773ace34c9ee58b8d1aa6855ed
Greg Omer <gregoryeomer@live.com>
2018-06-15 14:38:09 -0600

    added router export page

--- routes/notes.js
+++ routes/notes.js
@@ -7,12 +7,15 @@ router.get('/users/:id/notes/:id', ctrlNote.getOne)
 router.patch('/users/:id/notes/:id', ctrlNote.update)
 router.delete('/users/:id/notes/:id', ctrlNote.deleteOne)
 
+//////////////////////////////////////
+//////////////////////////////////////
 
+const ctrlUser = require('../controller/users.js')
 
-
-
-
-
+// router.post('/users', ctrlUser.create)
+router.get('/users/:id', ctrlUser.getOne)
+router.patch('/users/:id', ctrlUser.update)
+// router.delete('/users/:id', ctrlUser.deleteOne)
 
 
 
