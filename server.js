b974fef7a8c36a74ff238829662e73bc3b286a39
Ty Mondragon <tymondragon@gmail.com>
2018-06-15 14:01:58 -0600

    start build of server routes

--- server.js
+++ server.js
@@ -2,7 +2,50 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const app = express()
+const port = process.env.PORT || 3000
+const listener = () => {console.log(`Listening in on port ${port}.`)}
 
-app.use(static.public)
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+app.use(express.static("public"))
+//Auth Routes
+app.post('footnotes/users', (req, res, next)=> {
+  //create user account
+})
+app.post('footnotes/users', (req, res, next)=> {
+  //sign in as user
+})
+
+////USERS NOTES//
+app.get('footnotes/users/:id', (req, res, next) => {
+  //return users info
+})
+// app.delete('footnotes/users/:id', (req, res, next) => {
+//   //return users info
+// })
+app.patch('footnotes/users/:id', (req, res, next) => {
+  //update users info
+})
+
+
+/////NOTES ROUTER
+app.post('footnotes/users/:id/notes', (req, res, next) => {
+  //create a note
+})
+app.get('footnotes/users/:id/notes', (req, res, next) => {
+  //get all notes by user
+})
+app.get('footnotes/users/:id/notes/:id', (req, res, next) => {
+  //get specific note from user
+})
+app.patch('footnotes/users/:id/notes/:id', (req, res, next) => {
+  //update a note
+})
+app.delete('footnotes/users/:id/notes/:id', (req, res, next) => {
+  //delete a specific note
+})
+
+
+
