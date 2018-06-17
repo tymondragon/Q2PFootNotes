5ee12b21782160be4ac310483849d473a460c1b7
Ty Mondragon <tymondragon@gmail.com>
2018-06-17 15:29:17 -0600

    createUser createNote getNotes getOneUser done

--- routes/notes.js
+++ routes/notes.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const ctrl = require('../controller/notes.js')
-router.post('/users/:id/notes', ctrl.createNote)
-router.get('/users/:id/notes', ctrl.getNotes)
-router.get('/users/:id/notes/:id', ctrl.getOneNote)
+
+router.post('/:email/:password', ctrl.signIn)
+router.post('/notes', ctrl.createNote)
+router.get('/users/:id/notes', ctrl.getNotes)////////done//////
+router.get('/notes', ctrl.getAllNotes)////////done//////
+
+// router.get('/notes/:id', ctrl.getNotesBySubject)
 router.patch('/users/:id/notes/:id', ctrl.updateNote)
 router.delete('/users/:id/notes/:id', ctrl.deleteOneNote)
 
 //////////////////////////////////////
 //////////////////////////////////////
 
-router.post('/users', ctrl.createUser)
-router.get('/users/:id', ctrl.getOneUser)
+router.post('/users', ctrl.createUser)////////done/////
+router.get('/users/:id', ctrl.getOneUser)///////done/////
 router.patch('/users/:id', ctrl.updateUser)
 // const ctrlUser = require('../controller/users.js')
 //
