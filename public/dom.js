01a9807a20e2e56f6383f39a48f0c1bfe2ac6ea3
Ty Mondragon <tymondragon@gmail.com>
2018-06-18 14:55:29 -0600

    starting the ajax

---
+++ public/dom.js
@@ -0,0 +1,17 @@
+const footnotes = "http//localstorage:3000/"
+let notesId
+let userId
+$(document).ready(function() {
+      $('#textarea3 ').onload((e) => {
+            e.prevent.default()
+            $.ajax({
+                url: `${footnotes}notes/${userId}/${notesId}`,
+                type: 'GET',
+                data: $(this).serialize(),
+                success: (data) => {
+<a href="#!" class="collection-item">Alvin</a>
+                },
+                error: function(jqXhr, textStatus, errorThrown) {
+                  console.log('OOPS:', errorThrown)
+                })
+            })
