0afd0a5d57a6e2cb051146cfc48db16b707ef63f
Greg Omer <gregoryeomer@live.com>
2018-06-18 12:01:18 -0600

    merge conflict efforts

---
+++ migrations/20180616120921_users.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments()
+    table.string('first_name', 20).notNullable().defaultTo('')
+    table.string('last_name', 20).notNullable().defaultTo('')
+    table.string('email', 255).notNullable().defaultTo('')
+    table.varchar('hashed_pw', 255)
+    table.timestamps(true,true)
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users')
+};
