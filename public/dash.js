2bc699958930e9869978a85ba21d5831d543756b
Ty Mondragon <tymondragon@gmail.com>
2018-06-19 14:56:18 -0600

    working on populating list

--- public/dash.js
+++ public/dash.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   const user = JSON.parse(localStorage.getItem('userLogin'))
   let userId = user.id;
-  $.get( `http://localhost:3000/footnotes/notes/${userId}`, (data) => {
-  console.log(data);
+  $.get(`http://localhost:3000/footnotes/notes/${userId}`, (data) => {
+    console.log(data);
+  })
 })
-})
+
+
+
+// <li class="collection-item"><div>Alvin<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
