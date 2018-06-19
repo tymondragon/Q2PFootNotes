b5fa3fdaf3cc735c3e9a455d6797c2caa14ad1cd
Ty Mondragon <tymondragon@gmail.com>
2018-06-19 14:18:07 -0600

    working on first page AGAIN

--- public/dom.js
+++ public/dom.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
       $('.modal').modal();
+      let password = $('#hashed_pw').val()
+      let confirm = $('#password2').val()
+      let email = $('#modalEmail').val()
+      let modalPass = $('#password').val()
       /////This is the create a user//////
       $('#login').click((event) => {
-          let password = $('#hashed_pw').val()
-          let confirm = $('#password2').val()
           event.preventDefault()
           if (password !== confirm || password === '' || confirm === '') {
             M.toast({html: 'Passwords Must Match!!'})
@@ -20,7 +22,7 @@ $(document).ready(function() {
               })
               .done((data) => {
                 console.log("yay! for objects", data)
-                localStorage.setItem('userLogin', JSON.stringify(`${data.id, data.first_name}`))
+                localStorage.setItem('userLogin', JSON.stringify({id:data.id, name:data.first_name}))
                 window.location.href = './dashboard.html'
               })
               .fail(function(jqXhr, textStatus, errorThrown) {
@@ -28,4 +30,29 @@ $(document).ready(function() {
               })
           }
         })
+        $('#modalLogin').click((event) => {
+          event.preventDefault()
+          console.log(email, modalPass)
+        if (modalPass === '' || email === '') {
+          M.toast({html: 'Please Enter Valid Credentials'})
+        } else {
+          $.ajax({
+              url: 'http://localhost:3000/footnotes/login',
+              type: 'POST',
+              data: {
+                email: $('#modalEmail').val(),
+                hashed_pw: $('#password2').val()
+              },
+              dataType: 'json'
+            })
+            .done((data) => {
+              console.log("User has signed in", data)
+              localStorage.setItem('userLogin', JSON.stringify({id:data.id, name:data.first_name}))
+                // window.location.href = './dashboard.html'
+            })
+            .fail(function(jqXhr, textStatus, errorThrown) {
+              console.log('OOPS:', errorThrown)
+            })
+        }
+      })
       }); /////end of the doc .ready function
